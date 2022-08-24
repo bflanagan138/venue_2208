8e4c43a50d047cd90ea3c4b12e7bf1f592583e6b
Bryan Flanagan <bflanagan78@yahoo.com>
2022-08-24 09:42:25 -0600

    Add iteration 2

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,7 +1,17 @@
 class Venue
-  attr_reader :name, :capacity
+  attr_reader :name, :capacity, :patrons
   def initialize(name, capacity)
     @name = name
     @capacity = capacity
+    @patrons = []
+  end
+  def add_patron(patron)
+      @patrons << patron
+  end
+  # require 'pry'; binding.pry
+  def yell_at_patrons
+    @patrons.each do |patron|
+      patron.upcase!
+    end
   end
 end
